dd579e61eed9e1916304b27fa586bfb54559c325
rdebroiz <rdebroiz@openmailbox.org>
2017-04-19 17:24:05 +0200

    Failed Scope value are now pretty-formated when printed.

--- executor.py
+++ executor.py
@@ -12,6 +12,7 @@ import sys
 import subprocess
 import os
 import datetime
+from pprint import pformat
 
 from collections import OrderedDict
 
@@ -240,4 +241,4 @@ class ThreadedPipelineExecutor(PipelineExecutor):
         # print new line
         print("")
         if scope_values_failed:
-            logging.error("Failed scope value: \n%s", scope_values_failed)
+            logging.error("Failed scope value: \n%s", pformat(scope_values_failed))
